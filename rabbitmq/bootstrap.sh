30a35069ce7c34446cc41b71b325a50f6c2ccee1
Joost van de Wijgerd <jwijgerd@gmail.com>
2015-05-20 13:55:51 +0200

    added erlang repo so the latest stable erlang version can be used

--- rabbitmq/bootstrap.sh
+++ rabbitmq/bootstrap.sh
@@ -3,7 +3,11 @@ set -e
 
 echo "Installing RabbitMQ repo for Debian Wheezy"
 
+echo "deb http://packages.erlang-solutions.com/debian wheezy contrib" | tee -a /etc/apt/sources.list
+wget -qO - http://packages.erlang-solutions.com/debian/erlang_solutions.asc | sudo apt-key add -
 echo "deb http://www.rabbitmq.com/debian/ testing main" | tee -a /etc/apt/sources.list
 wget -qO - http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | sudo apt-key add -
 apt-get update
-apt-get -y install rabbitmq-server
+apt-get -y install erlang-base-hipe rabbitmq-server
+rabbitmq-plugins enable rabbitmq_management
+/etc/init.d/rabbitmq-server restart
