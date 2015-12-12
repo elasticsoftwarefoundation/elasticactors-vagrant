b3fbd4a4979c74f6e8f1750d74afda3c09598710
Joost van de Wijgerd <jwijgerd@gmail.com>
2015-12-12 14:59:38 +0100

    now installing Java 8 and Cassandra 2.2.4

--- cassandra/bootstrap.sh
+++ cassandra/bootstrap.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Installing Oracle JDK 7"
+echo "Installing Oracle JDK 8"
 
 apt-get -y install python-software-properties
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 apt-get -q -y update
 
 echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
 
-apt-get -y install oracle-java7-installer
+apt-get -y install oracle-java8-installer
 
-echo -e "\n\nJAVA_HOME=/usr/lib/jvm/java-7-oracle" >> /etc/environment;
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
+echo -e "\n\nJAVA_HOME=/usr/lib/jvm/java-8-oracle" >> /etc/environment;
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
 
-echo "Installing Cassandra 1.2 repo for Debian Wheezy"
+echo "Installing Cassandra 2.2 repo for Debian Jessie"
 
 echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
 wget -qO - http://debian.datastax.com/debian/repo_key | sudo apt-key add -
 apt-get update
 
-echo "Installing JNA and Cassandra 1.2"
+echo "Installing JNA and Cassandra 2.2"
 
 apt-get -y install libjna-java
-apt-get -y install dsc12=1.2.13-1 cassandra=1.2.13
+apt-get -y install dsc22=2.2.4-1 cassandra=2.2.4
 
 # copy the configuration to /etc/cassandra/
 echo "Creating cassandra configuration and schema"
@@ -37,4 +37,4 @@ rm -rf /var/lib/cassandra/*
 cp /vagrant/cassandra/cassandra.yaml /etc/cassandra
 /etc/init.d/cassandra start
 sleep 30
-cassandra-cli -h 192.168.56.2 -f /vagrant/cassandra/elasticactors.ddl
+cqlsh -h 192.168.56.2 -f /vagrant/cassandra/elasticactors.cql
